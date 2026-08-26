f811d2c1291e0dac77a6ed13ab5f3cec3f537717
Imam Mehedi Priyo <122119980+priyo394@users.noreply.github.com>
2026-08-26 13:38:25 +0600

    MindPulse-V2

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,11 +1,15 @@
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
+if (!admin.apps || admin.apps.length === 0) {
+  if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+    throw new Error("Missing Firebase Admin Environment Variables");
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
 }
